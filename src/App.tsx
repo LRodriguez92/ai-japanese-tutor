3cc860fd7568190bfde4f11db69c3ba27d12bc5f
Leo <Leonardo.Rodriguez92@gmail.com>
2024-03-19 09:42:55 -0400

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import './App.css';
 import FlashcardPage from './pages/FlashcardPage';
 import WritingCanvas from './components/WritingCanvas';
 import Chat from './components/Chat';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chat />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/flashcards">Flashcards</Link>
+            </li>
+            <li>
+              <Link to="/chat">Chat</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/flashcards" element={<FlashcardPage />} />
+          <Route path="/chat" element={<Chat />} />
+          {/* Redirect to FlashcardPage as the default route */}
+          <Route path="*" element={<FlashcardPage />} />
+        </Routes>
       </header>
     </div>
   );
