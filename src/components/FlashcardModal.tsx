7217d1ddd0204789c0c28d64de62f204f260aff0
Leo <Leonardo.Rodriguez92@gmail.com>
2024-04-17 15:09:38 -0400

    fixed component name in modal components

---
+++ src/components/FlashcardModal.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './FlashcardModal.css';
+
+interface InfoModalProps {
+  onClose: () => void;
+}
+
+const FlashcardModal: React.FC<InfoModalProps> = ({ onClose }) => {
+  const [doNotShowAgain, setDoNotShowAgain] = useState<boolean>(false);
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDoNotShowAgain(event.target.checked);
+  };
+
+  const handleClose = () => {
+    if (doNotShowAgain) {
+      localStorage.setItem('showFlashcardModal', 'false');
+    }
+    onClose();
+  };
+
+  return (
+    <div className="modal-background">
+      <div className="modal-content">
+        <h3>Let's learn some Kana!</h3>
+        <p>This interactive chat feature allows you to converse with an AI language instructor. You can type your messages in either english or japanese (you might have to enable a japanese keyboard on your device).
+        <br />
+        <br />
+        For added learning, you can tap on any response from the AI to see its translation. This feature is designed to enhance your language learning experience by providing instant translations and engaging conversations.
+        <br />
+        <br />
+        Remember, you can always revisit this info by accessing the help section in the chat.
+        </p>
+        <div className="modal-footer">
+          <label>
+            <input type="checkbox" checked={doNotShowAgain} onChange={handleCheckboxChange} />
+            Don't show this again
+          </label>
+          <button onClick={handleClose}>Close</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlashcardModal;
